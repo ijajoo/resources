dd0bde1edf605c509a1d56a601dbaec52f317601
ishajajoo <89950769+ijajoo@users.noreply.github.com>
2021-09-05 15:25:54 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,3 +8,7 @@ print (hello, world!)
 print ("hello, world!)
 #There will be a syntax error if you don't have both quotation marks
 
+#3
+2 + +2
+#In python putting two plus signs does the same thing as putting one.
+

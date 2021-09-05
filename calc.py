8b78d3dd65e8dbc69c7ac5cc6ee60da4a07a7648
ishajajoo <89950769+ijajoo@users.noreply.github.com>
2021-09-04 22:12:42 -0400

    Question 3

--- calc.py
+++ calc.py
@@ -1,8 +1,15 @@
 #1
-numOfSec= (42 * 60) + 42
-print(numOfSec)
+sec= (42 * 60) + 42
+print(sec)
 
 #2
 mileInKm = 1.61
 TotMiles = 10 / mileInKm
 print(TotMiles)
+
+#3
+pace = (sec / TotMiles) / 60
+print(pace)
+print (( (TotMiles / sec) *60) *60)
+
+ 

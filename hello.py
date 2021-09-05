29a59ae0e6452fc74aa73edf9d50aa9f9f865ec8
ishajajoo <89950769+ijajoo@users.noreply.github.com>
2021-09-05 15:22:08 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,8 @@ print( "hello, world!"
 print "hello, world!"
 #There is a syntax error if missing parenthesis
 
+#2
+print (hello, world!)
+print ("hello, world!)
+#There will be a syntax error if you don't have both quotation marks
+

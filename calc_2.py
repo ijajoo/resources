b95b930a5878f38f6959db32df1ea075c52c39ce
ishajajoo <89950769+ijajoo@users.noreply.github.com>
2021-09-08 21:07:45 -0400

    Question 2
    Calculating price of books.

--- calc_2.py
+++ calc_2.py
@@ -3,3 +3,12 @@ pi = 3.1415962
 radius = 5
 volume = (4/3) * pi * pow(radius,3)
 print('The volume of a sphere with a radius of', radius, 'is', volume)
+
+#Question 2
+price = 24.95
+shipping1 = 3
+shipping2 = .75
+n = 60
+finalPrice = (price * n ) + shipping1 + (shipping2 * (n - 1))
+print('The price of 60 books is', finalPrice, 'dollars')
+
